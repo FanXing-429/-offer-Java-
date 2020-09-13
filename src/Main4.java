088db7953b9ec10b38a3c1ebdefbab8ce86483ec
FanXing-429 <1534949714@qq.com>
2020-09-13 12:54:01 +0800

    重建二叉树

--- src/Main4.java
+++ src/Main4.java
@@ -7,7 +7,6 @@ public class Main4 {
     例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，
     则重建二叉树并返回。
     */
-
     public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
         //开始先判断一些特殊情况
         if(pre == null || pre.length == 0){
